c6350e00dc7f74ee07cf80c9abeb1fcfa85920e4
chaney <ymps3502@gmail.com>
2018-01-01 03:26:36 +0800

    add some preset in TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,33 @@
 
 namespace Tests;
 
+use Artisan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Initial database and make migration
+     */
+    protected function initDatabase()
+    {
+        config([
+            'database.default' => 'sqlite',
+            'database.connections.sqlite' => [
+                'driver' => 'sqlite',
+                'database' => ':memory:',
+                'prefix' => ''
+            ]
+        ]);
+
+        Artisan::call('migrate');
+        Artisan::call('db:seed');
+    }
+
+    protected function resetDatabase()
+    {
+        Artisan::call('migrate:reset');
+    }
 }
