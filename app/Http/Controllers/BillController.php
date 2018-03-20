119de711fa08ae1d7e749c7a7694e7c4d572b052
chaney <ymps3502@gmail.com>
2018-03-20 12:34:46 +0800

    base

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -15,22 +15,6 @@ class BillController extends Controller
         $this->repository = $repository;
     }
 
-    public function allCost()
-    {
-        $bills = $this->repository->allCost();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(Request $request)
-    {
-        $this->repository->create($request->all());
-        return response()->json(['message' => 'add bill successfuly'], 201);
-    }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -39,51 +23,191 @@ class BillController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->repository->create($request->all());
+        return response()->json(['msg' => 'add bill successfully'], 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Bill  $bill
-     * @return \Illuminate\Http\Response
+     * @return \App\Bill
      */
-    public function show(Bill $bill)
+    public function showAllCost()
     {
-        //
+        $bills = $this->repository->allCost();
+        return $bills;
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Display the specified resource.
      *
-     * @param  \App\Bill  $bill
-     * @return \Illuminate\Http\Response
+     * @param  int $id
+     * @return \App\Bill
      */
-    public function edit(Bill $bill)
+    public function showTagCost(int $id)
     {
-        //
+        $bills = $this->repository->tagCost($id);
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \App\Bill
+     */
+    public function showTodayCost()
+    {
+        $bills = $this->repository->todayCost();
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \App\Bill
+     */
+    public function showTodayTagCost()
+    {
+        $bills = $this->repository->todayCostByTag();
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param  string $role
+     * @return \App\Bill
+     */
+    public function showTodayRoleCost(string $role)
+    {
+        $bills = $this->repository->todayCostByRole($role);
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \App\Bill
+     */
+    public function showWeekCost()
+    {
+        $bills = $this->repository->weekCost();
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \App\Bill
+     */
+    public function showWeekTagCost()
+    {
+        $bills = $this->repository->weekCostByTag();
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param  string $role
+     * @return \App\Bill
+     */
+    public function showWeekRoleCost(string $role)
+    {
+        $bills = $this->repository->weekCostByRole($role);
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \App\Bill
+     */
+    public function showMonthCost()
+    {
+        $bills = $this->repository->monthCost();
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \App\Bill
+     */
+    public function showMonthTagCost()
+    {
+        $bills = $this->repository->monthCostByTag();
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string $role
+     * @return \App\Bill
+     */
+    public function showMonthRoleCost(string $role)
+    {
+        $bills = $this->repository->monthCostByRole($role);
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \App\Bill
+     */
+    public function showYearCost()
+    {
+        $bills = $this->repository->yearCost();
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \App\Bill
+     */
+    public function showYearTagCost()
+    {
+        $bills = $this->repository->yearCostByTag();
+        return $bills;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string $role
+     * @return \App\Bill
+     */
+    public function showYearRoleCost(string $role)
+    {
+        $bills = $this->repository->yearCostByRole($role);
+        return $bills;
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Bill  $bill
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Bill $bill)
+    public function update(Request $request, int $id)
     {
-        //
+        $this->repository->update($id, $request->all());
+        return response()->json(['msg' => 'update bill successfully'], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Bill  $bill
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Bill $bill)
+    public function destroy(int $id)
     {
-        //
+        $this->repository->delete($id);
+        return response()->json(['msg' => 'delete bill successfully'], 200);
     }
 }
