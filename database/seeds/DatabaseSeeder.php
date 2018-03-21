f904d49197eaa0154b7b6f989203c2efeae674e7
chaney <ymps3502@gmail.com>
2018-03-22 03:34:51 +0800

    create seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Tests\Unit\SeedData;
 
 class DatabaseSeeder extends Seeder
 {
+    use SeedData;
     /**
      * Run the database seeds.
      *
@@ -11,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $tables = array_map('current', DB::select('SHOW TABLES'));
+        foreach ($tables as $table) {
+            //if you don't want to truncate migrations
+            if ($table == 'migrations') {
+                continue;
+            }
+            DB::table($table)->truncate();
+        }
+        $this->seedData("thisYear");
     }
 }
